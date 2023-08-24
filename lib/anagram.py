0e79f5fcdb0827ce84244de1e674d18689db0dc0
Joy Olago <joydanielle62@gmail.com>
2023-08-24 13:32:58 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+       
+    def match(self, word_list):
+        anagrams = []
+
+        for letter in word_list:
+            if letter.lower() != self.word and sorted(letter.lower()) == sorted(self.word):
+                anagrams.append(letter)
+
+        return anagrams
+          
